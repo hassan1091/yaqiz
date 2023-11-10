26b129600f6297ec76feb01a0932b0d5bd326f81
حسن الخليف <roh.baba.11@gmail.com>
2023-11-10 22:13:43 +0300

    improve the performance of home page

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -20,12 +20,101 @@ class _HomePageState extends State<HomePage> {
           padding: const EdgeInsets.symmetric(horizontal: 16.0),
           children: [
             const SizedBox(height: kToolbarHeight),
-            _buildHeader(context),
-            _buildGreeting(),
+            SizedBox(
+              height: kToolbarHeight,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.person_outline),
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.login_outlined),
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const LoginPage(),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+            const Text(
+                "Hi, Anas",
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
             const SizedBox(height: 12),
-            _buildRemainderCard(),
+            Card(
+              shape: ContinuousRectangleBorder(
+                borderRadius: BorderRadius.circular(42),
+              ),
+              clipBehavior: Clip.hardEdge,
+              elevation: 10,
+              child: Column(
+                children: [
+                  const CustomGradientBackground(
+                    child: Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [Text("Remainder"), Text("28 Sep")],
+                      ),
+                    ),
+                  ),
+                  CustomGradientBackground(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      child: ListView.builder(
+                        itemCount: 4,
+                        padding: const EdgeInsets.only(bottom: 16),
+                        shrinkWrap: true,
+                        itemBuilder: (context, index) =>
+                            Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 8.0),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  InkWell(
+                                    child: Text(
+                                      "Visit bed $index",
+                                      style: const TextStyle(
+                                        color: Colors.blue,
+                                        fontStyle: FontStyle.italic,
+                                        decoration: TextDecoration.underline,
+                                      ),
+                                    ),
+                                  ),
+                                  const Text(
+                                    "01:51:39",
+                                    style: TextStyle(color: Colors.red),
+                                  ),
+                                ],
+                              ),
+                            ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
             const SizedBox(height: 24),
-            _buildBedsCardHead(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "Beds",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                InkWell(onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => const BedsPage(),));
+                },
+                    child: const Text("Show more")),
+              ],
+            ),
             const SizedBox(height: 12),
             const BedCardGrid()
           ],
@@ -34,109 +123,5 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Widget _buildHeader(BuildContext context) {
-    return SizedBox(
-      height: kToolbarHeight,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          IconButton(
-            icon: const Icon(Icons.person_outline),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: const Icon(Icons.login_outlined),
-            onPressed: () {
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const LoginPage(),
-                ),
-              );
-            },
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildGreeting() {
-    return const Text(
-      "Hi, Anas",
-      style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-    );
-  }
-
-  Widget _buildRemainderCard() {
-    return Card(
-      shape: ContinuousRectangleBorder(
-        borderRadius: BorderRadius.circular(42),
-      ),
-      clipBehavior: Clip.hardEdge,
-      elevation: 10,
-      child: Column(
-        children: [
-          const CustomGradientBackground(
-            child: Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [Text("Remainder"), Text("28 Sep")],
-              ),
-            ),
-          ),
-          CustomGradientBackground(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: ListView.builder(
-                itemCount: 4,
-                padding: const EdgeInsets.only(bottom: 16),
-                shrinkWrap: true,
-                itemBuilder: (context, index) => Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      InkWell(
-                        child: Text(
-                          "Visit bed $index",
-                          style: const TextStyle(
-                            color: Colors.blue,
-                            fontStyle: FontStyle.italic,
-                            decoration: TextDecoration.underline,
-                          ),
-                        ),
-                      ),
-                      const Text(
-                        "01:51:39",
-                        style: TextStyle(color: Colors.red),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildBedsCardHead() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        const Text(
-          "Beds",
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-        ),
-        InkWell(onTap: (){
-          Navigator.push(context, 
-          MaterialPageRoute(builder: (context) => const BedsPage(),));
-        },
-            child: const Text("Show more")),
-      ],
-    );
-  }
 }
 
