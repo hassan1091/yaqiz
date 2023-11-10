37e496ba9b6088b05b8449e05c81e3c0e5ebe63e
حسن الخليف <roh.baba.11@gmail.com>
2023-11-10 18:37:20 +0300

    make build function for header and greeting widgets

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -3,7 +3,7 @@ import 'package:yaqiz/page/login.dart';
 import 'package:yaqiz/widget/custom_gradient_background.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -14,39 +14,50 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: CustomGradientBackground(
-        child: Padding(
+        child: ListView(
           padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: kToolbarHeight),
-              SizedBox(
-                  height: kToolbarHeight,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      IconButton(
-                          icon: const Icon(Icons.person_outline),
-                          onPressed: () {}),
-                      IconButton(
-                          icon: const Icon(Icons.login_outlined),
-                          onPressed: () {
-                            Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => const LoginPage(),
-                                ));
-                          })
-                    ],
-                  )),
-              const Text(
-                "Hi, Ans",
-                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-              ),
-            ],
-          ),
+          children: [
+            const SizedBox(height: kToolbarHeight),
+            _buildHeader(context),
+            _buildGreeting(),
+          ],
         ),
       ),
     );
   }
+
+  Widget _buildHeader(BuildContext context) {
+    return SizedBox(
+      height: kToolbarHeight,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(
+            icon: const Icon(Icons.person_outline),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(Icons.login_outlined),
+            onPressed: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const LoginPage(),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildGreeting() {
+    return const Text(
+      "Hi, Anas",
+      style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+    );
+  }
+
+
 }
