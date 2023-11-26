a83824558b4d0052267882933c5c097e92822616
حسن الخليف <roh.baba.11@gmail.com>
2023-11-26 15:33:45 +0300

    create api constants

---
+++ lib/constant.dart
@@ -0,0 +1,5 @@
+class ApiConstants{
+  static String androidBaseUrl = 'http://10.0.2.2:8080';
+  static String iOSBaseUrl = 'http://127.0.0.1:8080';
+  static const String userEndpoint = "/user";
+}
