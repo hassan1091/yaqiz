d53f9131cd162b0186ef8416392ba0dfc984df95
حسن الخليف <roh.baba.11@gmail.com>
2023-11-26 17:56:38 +0300

    create shared preferences

---
+++ lib/shared_preferences.dart
@@ -0,0 +1,36 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+enum AppStorageKey { id, admin }
+
+class AppLocalStorage {
+  static SharedPreferences? _prefs;
+
+  static Future<SharedPreferences> get prefs async {
+    _prefs ??= await SharedPreferences.getInstance();
+    return _prefs!;
+  }
+
+  static Future<void> setString(AppStorageKey key, String value) async {
+    (await prefs).setString(key.toString(), value);
+  }
+
+  static Future<String?> getString(AppStorageKey key) async {
+    return (await prefs).getString(key.toString());
+  }
+
+  static Future<void> setBool(AppStorageKey key, bool value) async {
+    (await prefs).setBool(key.toString(), value);
+  }
+
+  static Future<bool?> getBool(AppStorageKey key) async {
+    return (await prefs).getBool(key.toString());
+  }
+
+  static Future<bool> isExist(AppStorageKey key) async {
+    return (await prefs).containsKey(key.toString());
+  }
+
+  static Future<void> delete(AppStorageKey key) async {
+    (await prefs).remove(key.toString());
+  }
+}
