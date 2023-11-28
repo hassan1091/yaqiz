8d02564decefc70ca7826faa656098614c3d89b8
حسن الخليف <roh.baba.11@gmail.com>
2023-11-29 00:20:08 +0300

    reformat

--- lib/api/medel/device.dart
+++ lib/api/medel/device.dart
@@ -7,12 +7,13 @@ class Device {
 
   factory Device.fromJson(Map<String, dynamic> json) {
     return Device(
-        id: json['Device_ID'],
-        location: json['Device_Location'],
-        employeeId: json['Employee_ID']);
+      id: json['Device_ID'],
+      location: json['Device_Location'],
+      employeeId: json['Employee_ID'],
+    );
   }
 
-  Map<String, dynamic> toJson() {
+  Future<Map<String, dynamic>> toJson() async {
     return {
       "Device_ID": id,
       "Device_Location": location,
