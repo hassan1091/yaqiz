cdcd76aaf8d240707e87835797234a7c450564b3
حسن الخليف <roh.baba.11@gmail.com>
2023-11-28 21:14:05 +0300

    implement get user api

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -31,147 +31,162 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: CustomGradientBackground(
-        child: ListView(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          children: [
-            const SizedBox(height: kToolbarHeight),
-            SizedBox(
-              height: kToolbarHeight,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton(
-                    icon: const Icon(Icons.person_outline),
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const ContactPage()));
-                    },
-                  ),
-                  IconButton(
-                    icon: const Icon(Icons.login_outlined),
-                    onPressed: _logout,
-                  ),
-                ],
-              ),
-            ),
-            const Text("Hi, Anas",
-                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
-            const SizedBox(height: 12),
-            if (widget.isAdmin) ...[
-              const Text("Doctors",
-                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
-              SizedBox(
-                height: 100,
-                child: ListView.separated(
-                  itemCount: 10,
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (context, index) => Column(
+        child: FutureBuilder(
+          future: ApiService().getUser(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            }
+            return ListView(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              children: [
+                const SizedBox(height: kToolbarHeight),
+                SizedBox(
+                  height: kToolbarHeight,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       IconButton(
-                          iconSize: 64,
-                          padding: EdgeInsets.zero,
-                          color: Colors.blue,
-                          onPressed: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => const StaffPage()));
-                          },
-                          icon: const Icon(
-                            CupertinoIcons.person_alt_circle,
-                          )),
-                      Text("Staff $index")
+                        icon: const Icon(Icons.person_outline),
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const ContactPage()));
+                        },
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.login_outlined),
+                        onPressed: _logout,
+                      ),
                     ],
                   ),
-                  separatorBuilder: (BuildContext context, int index) =>
-                      const SizedBox(width: 12),
                 ),
-              )
-            ],
-            Card(
-              shape: ContinuousRectangleBorder(
-                borderRadius: BorderRadius.circular(42),
-              ),
-              clipBehavior: Clip.hardEdge,
-              elevation: 10,
-              child: Column(
-                children: [
-                  CustomGradientBackground(
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                Text("Hi, ${snapshot.data!.employeeID}",
+                    style: const TextStyle(
+                        fontSize: 28, fontWeight: FontWeight.bold)),
+                const SizedBox(height: 12),
+                if (widget.isAdmin) ...[
+                  const Text("Doctors",
+                      style:
+                          TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
+                  SizedBox(
+                    height: 100,
+                    child: ListView.separated(
+                      itemCount: 10,
+                      shrinkWrap: true,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) => Column(
                         children: [
-                          const Text("Remainder"),
-                          Text("${DateTime.now().day}/${DateTime.now().month}")
+                          IconButton(
+                              iconSize: 64,
+                              padding: EdgeInsets.zero,
+                              color: Colors.blue,
+                              onPressed: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const StaffPage()));
+                              },
+                              icon: const Icon(
+                                CupertinoIcons.person_alt_circle,
+                              )),
+                          Text("Staff $index")
                         ],
                       ),
+                      separatorBuilder: (BuildContext context, int index) =>
+                          const SizedBox(width: 12),
                     ),
+                  )
+                ],
+                Card(
+                  shape: ContinuousRectangleBorder(
+                    borderRadius: BorderRadius.circular(42),
                   ),
-                  ValueListenableBuilder(
-                    valueListenable: alarms,
-                    builder: (context, value, child) =>
-                        CustomGradientBackground(
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: ListView.builder(
-                          itemCount: value.length,
-                          padding: const EdgeInsets.only(bottom: 16),
-                          shrinkWrap: true,
-                          itemBuilder: (context, index) => Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 8.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                InkWell(
-                                  child: Text(
-                                    "Visit ${value[index].title}",
-                                    style: const TextStyle(
-                                      color: Colors.blue,
-                                      fontStyle: FontStyle.italic,
-                                      decoration: TextDecoration.underline,
+                  clipBehavior: Clip.hardEdge,
+                  elevation: 10,
+                  child: Column(
+                    children: [
+                      CustomGradientBackground(
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text("Remainder"),
+                              Text(
+                                  "${DateTime.now().day}/${DateTime.now().month}")
+                            ],
+                          ),
+                        ),
+                      ),
+                      ValueListenableBuilder(
+                        valueListenable: alarms,
+                        builder: (context, value, child) =>
+                            CustomGradientBackground(
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: ListView.builder(
+                              itemCount: value.length,
+                              padding: const EdgeInsets.only(bottom: 16),
+                              shrinkWrap: true,
+                              itemBuilder: (context, index) => Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 8.0),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    InkWell(
+                                      child: Text(
+                                        "Visit ${value[index].title}",
+                                        style: const TextStyle(
+                                          color: Colors.blue,
+                                          fontStyle: FontStyle.italic,
+                                          decoration: TextDecoration.underline,
+                                        ),
+                                      ),
                                     ),
-                                  ),
+                                    Text(
+                                      "${value[index].alarmDateTime.hour}:${value[index].alarmDateTime.minute}",
+                                      style: const TextStyle(color: Colors.red),
+                                    ),
+                                  ],
                                 ),
-                                Text(
-                                  "${value[index].alarmDateTime.hour}:${value[index].alarmDateTime.minute}",
-                                  style: const TextStyle(color: Colors.red),
-                                ),
-                              ],
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
+                    ],
                   ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 24),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  "Beds",
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                 ),
-                InkWell(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const BedsPage(),
-                          ));
-                    },
-                    child: const Text("Show more")),
+                const SizedBox(height: 24),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      "Beds",
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                    InkWell(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const BedsPage(),
+                              ));
+                        },
+                        child: const Text("Show more")),
+                  ],
+                ),
+                const SizedBox(height: 12),
+                const BedCardGrid()
               ],
-            ),
-            const SizedBox(height: 12),
-            const BedCardGrid()
-          ],
+            );
+          },
         ),
       ),
     );
