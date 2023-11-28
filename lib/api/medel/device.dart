3b61f2574f254ccf230ac374a08f53bc9d4dcf7c
حسن الخليف <roh.baba.11@gmail.com>
2023-11-29 00:04:48 +0300

    create device model

---
+++ lib/api/medel/device.dart
@@ -0,0 +1,22 @@
+class Device {
+  final int? id;
+  final String? location;
+  final int? employeeId;
+
+  Device({this.id, this.location, this.employeeId});
+
+  factory Device.fromJson(Map<String, dynamic> json) {
+    return Device(
+        id: json['Device_ID'],
+        location: json['Device_Location'],
+        employeeId: json['Employee_ID']);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      "Device_ID": id,
+      "Device_Location": location,
+      "Employee_ID": employeeId,
+    };
+  }
+}
